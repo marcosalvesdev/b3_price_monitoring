379691d37da9047fbc677afa9cd2c95d94aa9f9a
Marcos <marcosalves.dev@gmail.com>
2026-02-09 12:44:53 -0300

    feat(tunnels-app-crud-creation): Add asset detail page and update asset list with view link

--- assets/urls.py
+++ assets/urls.py
@@ -10,8 +10,8 @@ app_name = 'assets'
 
 urlpatterns = [
     path('', AssetListView.as_view(), name='list'),
-    path('detail/<int:pk>/', AssetDetailView.as_view(), name='detail'),
+    path('<int:pk>/', AssetDetailView.as_view(), name='detail'),
     path('create/', AssetCreateView.as_view(), name='create'),
-    path('update/<int:pk>/', AssetUpdateView.as_view(), name='update'),
-    path('delete/<int:pk>/', AssetDeleteView.as_view(), name='delete'),
+    path('<int:pk>/update/', AssetUpdateView.as_view(), name='update'),
+    path('<int:pk>/delete/', AssetDeleteView.as_view(), name='delete'),
 ]
