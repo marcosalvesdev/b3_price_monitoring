07aa7d6e56891cfcbf79b99a8302690a90561d52
Marcos <marcosalves.dev@gmail.com>
2026-02-18 14:20:40 -0300

    feat(db-connection-config): Add .env-example for environment variables and update settings for PostgreSQL

--- core/settings.py
+++ core/settings.py
@@ -81,8 +81,12 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("POSTGRES_DB"),
+        "USER": config("POSTGRES_USER"),
+        "PASSWORD": config("POSTGRES_PASSWORD"),
+        "HOST": config("POSTGRES_HOST", default="localhost"),
+        "PORT": config("POSTGRES_PORT", default="5432", cast=int),
     }
 }
 
