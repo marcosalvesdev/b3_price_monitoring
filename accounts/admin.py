01aa568b89800a4bdc1855766e1cf879a5667363
Marcos <marcosalves.dev@gmail.com>
2026-03-06 17:24:04 -0300

    feat(create-accounts-app): Add UserProfile model with avatar and signals for automatic profile creation

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from accounts.models import User
+from accounts.models import User, UserProfile
 
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin): ...
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "avatar")
--- core/urls.py
+++ core/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -30,4 +31,8 @@ urlpatterns = [
 if settings.DEBUG and not settings.RUNNING_TESTS:
     from debug_toolbar.toolbar import debug_toolbar_urls
 
-    urlpatterns += debug_toolbar_urls()
+    urlpatterns = [
+        *urlpatterns,
+        *debug_toolbar_urls(),
+        *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+    ]
