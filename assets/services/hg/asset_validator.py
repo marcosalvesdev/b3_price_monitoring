c3c75508a01ceeea11925253c71683759be480a2
Marcos <marcosalves.dev@gmail.com>
2026-03-08 15:30:11 -0300

    feat(create-development-md): Add DEVELOPMENT.md and remove unused HG API handler and asset validator files

--- assets/services/hg/asset_validator.py
+++
@@ -1,9 +0,0 @@
-from assets.utils.validators.base_asset_validator import BaseAssertValidator
-
-
-class HgApiAssetValidator(BaseAssertValidator):
-    def validate_asset(self) -> bool:
-        if not self.data.get("results", {}).get(self.symbol):
-            return False
-
-        return True
